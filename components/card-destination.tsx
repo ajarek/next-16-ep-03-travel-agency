024f1d0e2c3722a0759635fc34aa3f62b4748228
ajarek <ajarek@poczta.onet.pl>
2025-11-16 00:40:14 +0100

    added font-playfair-display

--- components/card-destination.tsx
+++ components/card-destination.tsx
@@ -15,29 +15,29 @@ type Destination = {
 const MotionCard = ({destinations }: {destinations:Destination[]}) => {
 
   return (
-    <motion.div
-      whileHover={{ scale: 1.1 }}
+    
+        <div className=' grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4 '>
+      {destinations.map((destination, index) => {
+        const { id, cityName, country, image, description, durationDays, priceUSD } = destination
+        return (
+          <motion.div
+      whileHover={{ scale: 1.1}}
       whileTap={{ scale: 0.95 }}
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
         duration: 3,
         scale: { type: 'spring', visualDuration: 2, bounce: 0.5 },
-      }}
+      }} key={id}
     >
-        <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4'>
-      {destinations.map((destination) => {
-        const { id, cityName, country, image, description, durationDays, priceUSD } = destination
-        return (
-          
-          <Card key={id} className='group shadow-zinc-950/5 bg-primary text-primary-foreground'>
-            <CardHeader className='pb-3'>
+          <Card  className='group shadow-zinc-950/5 bg-primary text-primary-foreground '>
+            <CardHeader className='relative   flex flex-col items-center justify-center '>
               <Image
                 src={image}
                 alt={cityName}
                 width={400}
                 height={400}
-                className='rounded-md mb-4 group-hover:opacity-80 transition-opacity duration-300'
+                className='rounded-md  group-hover:opacity-80 transition-opacity duration-300'
               />
               
             </CardHeader>
@@ -49,18 +49,22 @@ const MotionCard = ({destinations }: {destinations:Destination[]}) => {
               <p className='text-lg'>
                 {description}
               </p>
+              <div className='flex items-center justify-between'>
+
               <p className='mt-2 text-xl font-semibold'>
                 Price: {priceUSD} USD
               </p>
               <p className='text-md text-muted-foreground'>
                 Duration: {durationDays} days
               </p>
+                
+              </div>
             </CardContent>
           </Card>
+      </motion.div>
         )
       })}
       </div>
-    </motion.div>
   )
 }
 export default MotionCard
--- components/destinations-top.tsx
+++ components/destinations-top.tsx
@@ -2,17 +2,18 @@ import destinations from '@/data/destinations.json'
 import MotionCard from './card-destination'
 
 const DestinationsTop = () => {
+  const destinationsFilter = destinations.trips.filter((destination)=>destination.id<=3)
   return (
-    <section className='w-full  py-8 md:py-16 bg-transparent'>
-      <div className='@container mx-auto max-w-5xl px-6'>
+    <section className='w-full  py-8 md:py-16 px-8 bg-transparent '>
+      <div className='@container mx-auto max-w-7xl px-6'>
         <div className='text-center'>
-          <h2 className='text-balance text-4xl font-semibold lg:text-5xl'>
+          <h2 className='text-balance text-4xl font-semibold lg:text-5x mb-8'>
             Top Destinations
           </h2>
         </div>
        
 
-        <MotionCard destinations={Array.isArray(destinations) ? destinations : (destinations as any).trips} />
+        <MotionCard destinations={destinationsFilter} />
         </div>
         
         </section>
