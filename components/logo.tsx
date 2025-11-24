fe157ba360139c856ae78a037af0dc32b927f59a
ajarek <ajarek@poczta.onet.pl>
2025-11-25 00:14:33 +0100

    feat: add HeroSection and Logo components for the landing page.

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export const Logo = ({ className, uniColor }: { className?: string; uniColor?: boolean }) => {
     return (
-       <Image src={'/magellan-logo.png'} alt='logo' width={134} height={64}/>
+       <Image src={'/magellan-logo.png'} alt='logo' width={134} height={64} loading="eager"/>
     )
 }
 
